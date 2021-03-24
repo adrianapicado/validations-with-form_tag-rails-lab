baf38210d43866ded50a768a8b7b00b3d4abe408
adriana <picadoadriana7@gmail.com>
2021-03-23 20:17:08 -0400

    commit tree 0a3ea76b1ae0b08ea2e4e5e0367ef04ce127c91b

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,12 +10,18 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.create(author_params)
 
-    redirect_to author_path(@author)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
   end
+end
 
   private
 
   def author_params
     params.permit(:name, :email, :phone_number)
   end
+
 end
